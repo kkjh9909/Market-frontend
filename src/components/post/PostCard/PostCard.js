2e90b3110fc72bc1968a98a2ff0725ea7dbea479
kkjh9909 <kkjh990957@gmail.com>
2023-08-31 00:55:45 +0900

    중고물건 판매 조회 페이지 생성

---
+++ src/components/post/PostCard/PostCard.js
@@ -0,0 +1,14 @@
+
+export const PostCard = () => {
+	return (
+		<div className="card col-4">
+			<img src="/logo192.png" style={{height: "20vh"}} />
+			<div className="card-body">
+				<h5 className="card-title">card title</h5>
+				<p className="card-text">Some quick example text to build on the card title and make up the bulk
+					of the card's content.</p>
+				<a href="#" className="btn btn-primary">Go somewhere</a>
+			</div>
+		</div>
+	)
+}
---
+++ src/pages/FleaMarket.js
@@ -0,0 +1,86 @@
+import {useEffect, useState} from "react";
+import {useNavigate, useParams} from "react-router-dom";
+import {PostCard} from "../components/post/PostCard/PostCard";
+
+export const FleaMarket = () => {
+	const [region, setRegion] = useState('');
+
+	const params = useParams();
+	const navigate = useNavigate();
+
+	const regionKorean = {
+		seoul: '서울특별시',
+		busan: '부산광역시',
+		daegu: '대구광역시',
+		incheon: '인천광역시',
+		gwangju: '광주광역시',
+		daejeon: '대전광역시',
+		ulsan: '울산광역시',
+	}
+
+	const regions = [
+		{ value: '', label: '지역을 선택하세요' },
+		{ value: 'seoul', label: '서울특별시' },
+		{ value: 'busan', label: '부산광역시' },
+		{ value: 'daegu', label: '대구광역시' },
+		{ value: 'incheon', label: '인천광역시' },
+		{ value: 'gwangju', label: '광주광역시' },
+		{ value: 'daejeon', label: '대전광역시' },
+		{ value: 'ulsan', label: '울산광역시' },
+	]
+
+	useEffect(() => {
+		async function asd() {
+			const region = params.region;
+			if(!region)
+				console.log("선택하지 않음")
+			else {
+				console.log(params)
+			}
+			console.log(params)
+		}
+
+		asd();
+	}, [params.region])
+
+	const handleOptionChange = (e) => {
+		const selected = e.target.value;
+		setRegion(selected);
+		navigate(`/flea/${selected}`)
+	};
+
+	return (
+		<div className="container mt-lg-5">
+			<h1>{regionKorean[params.region] || ''} 인기 매물</h1>
+			<div className="w-100 row">
+				<div>
+					<div className="col">
+						<select
+							className="form-control w-25"
+							id="selectOption"
+							value={params.region}
+							onChange={handleOptionChange}
+						>
+						{regions.map(option => (
+							<option key={option.value} value={option.value}>
+								{option.label}
+							</option>
+						))}
+					</select>
+					</div>
+					<div className="col">
+					<input
+						placeholder="키워드를 입력하세요"
+						className="form-control w-25"
+					/>
+					</div>
+				</div>
+			</div>
+			<div className="row mt-lg-5" style={{clear: "both"}}>
+				<PostCard />
+				<PostCard />
+				<PostCard />
+			</div>
+		</div>
+	)
+}
